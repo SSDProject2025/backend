ba0e59e04780749f455251640b28d7fec6d2cffd
GiuseppeMattia <gmmezzotero@gmail.com>
2025-12-04 19:31:02 +0100

    Creata primitiva di dominio Pegi e relativi test

--- primitives/pegi.py
+++ primitives/pegi.py
@@ -3,19 +3,19 @@ from dataclasses import dataclass
 from typeguard import typechecked
 from valid8 import validate, ValidationError
 
-from primitives.exceptions import VoteException
+from primitives.exceptions import PegiException
 
 
 @typechecked
 @dataclass(frozen=True, order=True)
-class Vote:
-    vote: int
+class Pegi:
+    pegi_ranking_int: int
 
     def __post_init__(self):
         try:
-            validate("vote", self.vote, min_value=1, max_value=10)
+            validate("pegi_ranking", self.pegi_ranking_int, is_in=[3, 7, 12, 16, 18])
         except ValidationError:
-            raise VoteException
+            raise PegiException
 
     def __str__(self):
-        return str(self.vote)
+        return f"PEGI {self.pegi_ranking_int}"
