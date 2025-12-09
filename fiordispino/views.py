d19fd036832389e7d651be2d583054703bce0ec0
riccaaardo <cerrariccardo03@gmail.com>
2025-12-09 19:03:51 +0100

    added permission classrds

--- fiordispino/views.py
+++ fiordispino/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, viewsets
 from fiordispino.models import Genre, Game
 from fiordispino.serializers import GenreSerializer
 from fiordispino.serializers.game_serializer import GameSerializer
+from fiordispino import permissions
 
 '''
 class GenreList(generics.ListCreateAPIView):
@@ -16,9 +17,11 @@ class GenreDetail(generics.RetrieveUpdateDestroyAPIView):
 
 # use a single view class to replace multiple views
 class GenreViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAdminOrReadOnly]
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GameViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAdminOrReadOnly]
     queryset = Game.objects.all()
     serializer_class = GameSerializer
