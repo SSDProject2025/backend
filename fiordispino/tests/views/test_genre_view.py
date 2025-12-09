01562d644a10963fbc0410f780d5d970c5a35a73
riccaaardo <cerrariccardo03@gmail.com>
2025-12-09 19:21:15 +0100

    expanded genre tests to use auth

--- fiordispino/tests/views/test_genre_view.py
+++ fiordispino/tests/views/test_genre_view.py
@@ -2,6 +2,7 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate
 from mixer.backend.django import mixer
+from django.contrib.auth.models import User  # Import necessario per creare l'admin
 from fiordispino.views import GenreViewSet
 from fiordispino.models import Genre
 
@@ -10,11 +11,16 @@ notes on view test:
 when you test url, you can istantiate an API client which simulate a call; when you test a view you have to manually configure the request.
 """
 
+
 @pytest.mark.django_db
 class TestGenreView:
 
-    def test_list_genres_view(self):
+    # Aggiungiamo la fixture per l'admin, come fatto per Game
+    @pytest.fixture
+    def admin_user(self):
+        return mixer.blend(User, is_staff=True, is_superuser=True)
 
+    def test_list_genres_view(self):
         # create dummy data
         mixer.blend(Genre, name="Action")
         mixer.blend(Genre, name="Adventure")
@@ -32,7 +38,7 @@ class TestGenreView:
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == 3
 
-    def test_create_genre_view(self):
+    def test_create_genre_view(self, admin_user):  # Passiamo admin_user qui
         # similar to the above method, but this time we're testing the creation
 
         factory = APIRequestFactory()
@@ -42,6 +48,9 @@ class TestGenreView:
         # if the view needed auth, you could use:
         # user = mixer.blend('auth.User')
         # force_authenticate(request, user=user)
+
+        # ORA APPLICHIAMO L'AUTENTICAZIONE REALE QUI SOTTO:
+        force_authenticate(request, user=admin_user)
 
         view = GenreViewSet.as_view({'post': 'create'})
         response = view(request)
