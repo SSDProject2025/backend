6269c49b64dfd6f16ddcf2a8074d3feff2bd51d7
riccaaardo <cerrariccardo03@gmail.com>
2025-12-10 18:28:46 +0100

    bug fix in genre urls test

--- fiordispino/tests/urls/test_genre_urls.py
+++ fiordispino/tests/urls/test_genre_urls.py
@@ -40,11 +40,11 @@ class TestGenreUrl:
         response = admin.get(path)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_admin_can_add(self, game_data):
+    def test_admin_can_add(self, admin_user, genre_data):
         path = reverse('genre-list')
-        client = get_client()
-        response = client.post(path, game_data)
-        assert response.status_code == status.HTTP_403_FORBIDDEN
+        client = get_admin(admin_user)
+        response = client.post(path, genre_data)
+        assert response.status_code == status.HTTP_201_CREATED
 
     def test_admin_can_delete(self, admin_user, genres):
         path = reverse('genre-detail', kwargs={'pk': genres[0].id})
