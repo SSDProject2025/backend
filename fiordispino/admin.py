3ccb5d8924e60f0e72795813c67b57925cdcbee9
riccaaardo <cerrariccardo03@gmail.com>
2025-12-14 19:35:20 +0100

    now global rating and rating count are read only fields for the admin

--- fiordispino/admin.py
+++ fiordispino/admin.py
@@ -4,8 +4,13 @@ from fiordispino.models import *
 from fiordispino.permissions import *
 
 admin.site.register(Genre)
-admin.site.register(Game)
 admin.site.register(User)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    # these fields can only change when a user make some action on GamesPlayed
+    readonly_fields = ('global_rating', 'rating_count')
 
 # The admin should just visualize libraries, not create/edit/delete
 @admin.register(GamesToPlay)
